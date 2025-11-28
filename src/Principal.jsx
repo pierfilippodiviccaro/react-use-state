5dc43b8b6113761dd7a856c4b578f706de563ded
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-28 18:40:59 +0100

    feat: aggiunto colore ai bottoni

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -8,12 +8,13 @@ export default function DevLanguages() {
             <h1>learn your new programming language</h1>
             {languages.map((language)=>{
                 return(
-                    <button onClick={()=>setShow(language)} className='btn flex btn-warning'>{language.title}</button>
+                    <button onClick={()=>setShow(language)} className='btn flex  btn-outline-warning mt-5'>{language.title}</button>
                 )
             })}
+            <div className="border border-warning mt-5">
             <h2>{show ? show.title : ""}</h2>
             <p>{show ? show.description : ""}</p>
-
+            </div>
         </div>
     )
 }
